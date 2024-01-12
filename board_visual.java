93061a02aabe82d823bf24aa15f44f190aedfc00
alexyesilyurt <yesilyurtalexander@gmail.com>
2024-01-12 14:20:24 -0500

    started website

--- board_visual.java
+++ board_visual.java
@@ -1,10 +1,20 @@
+import javax.swing.JFrame;
+
+
 public class board_visual {
     public static void main(String[] args) {
-        for (int i=0; i<10; i++) {
-            for (int j=0; j<10; j++) {
-                System.out.print("- ");
-            }
-            System.out.println();
-        }
+        // for (int i=0; i<10; i++) {
+        //     for (int j=0; j<10; j++) {
+        //         System.out.print("- ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // JFrame frame = new JFrame("Battleship");
+        // frame.setSize(700, 600);
+        // frame.setVisible(true);
+
+        // GridPane 
+
     }
 }
