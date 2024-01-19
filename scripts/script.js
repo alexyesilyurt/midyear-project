8c29a53e0b2f2ec399f3e550e69cfaa91fc11e92
alexyesilyurt <yesilyurtalexander@gmail.com>
2024-01-19 10:35:03 -0500

    site
    Co-authored-by: giannitsivouras <giannitsivouras@users.noreply.github.com>
    Co-authored-by: ThomasSoltys <ThomasSoltys@users.noreply.github.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
 // https://stackoverflow.com/questions/32772708/how-to-create-a-specific-xx-size-table-battleship-game-html-css-js
 
-var table = function(rows, cols) {
-    var yarray
+var createTable = function(rows, cols) {
+    var YArray = new Array(), 
+        XArray, 
+            container = $(".table");
+    for (var i = 0; i < cols; i++) {
+        XArray = new Array();
+        for (var j = 0; j < rows; j++) {
+            XArray[j] = '<div class="cell">&nbsp;</div>';
+        }
+        YArray[i] = '<div class="row">' + XArray.join("\r\n") + '</div>';
+    }
+    container.append(YArray.join("\r\n"));
 };
+
+createTable(10, 10);
