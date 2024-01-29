f2f8a3e1dac747c8626cc04f0061915d8ff57ee8
ThomasSoltys <thosol24@bergen.org>
2024-01-29 14:59:08 -0500

    continue to work on java script

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ const optionShips = console.log(Array.from(optionContainer.children))
 // Creating Boards
 const width = 10 
 
-function createBoard(color){
+function createBoard(color, user){
     const gameBoardContainer = document.createElement('div')
     gameBoardContainer.classList.add('game-board')
     gameBoardContainer.style.backgroundColor = color
@@ -35,7 +35,42 @@ createBoard('yellow', 'player')
 createBoard('pink', 'computer')
 
 
-flipbutton.addEventListener('click', flip)
+// Creating Ships 
+
+class Ship {
+    constructor(name, length) {
+        this.name = name
+        this.legnth = length
+    }
+}
+
+const destroyer = new Ship('destroy', 2)
+const submarine = new Ship('submarine', 3)
+const cruiser = new Ship('cruiser', 3)
+const battleship = new Ship('battleship', 4)
+const carrier = new Ship('carrier', 5) 
+
+const ships = [destroyer, submarine, cruiser, battleship, carrier]
+
+function addShipPiece(ship){
+    const allBoardBlocks = document.querySelector('#computer div')
+    let randomBoolean = Math.random() <0.5
+    let isHorizontal = randomBoolean
+    let randomStartIndex= Math.floor(math.random() *  width * width)
+    console.log(randomStartIndex)
+
+    for (let i=0; i<ship.length; i++){
+        if(isHorizontal){
+
+        }
+    }
+
+}
+
+
+
+
+addShipPiece()
 
 
 
