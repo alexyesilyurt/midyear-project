f29d40be3663a8d564afb49071668671c4ce5a03
ThomasSoltys <thosol24@bergen.org>
2024-01-29 12:33:09 -0500

    Worked on javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,39 @@
-// https://stackoverflow.com/questions/32772708/how-to-create-a-specific-xx-size-table-battleship-game-html-css-js
 
-var createTable = function(rows, cols) {
-    var YArray = new Array(), 
-        XArray, 
-            container = $(".table");
-    for (var i = 0; i < cols; i++) {
-        XArray = new Array();
-        for (var j = 0; j < rows; j++) {
-            XArray[j] = '<div class="cell">&nbsp;</div>';
-        }
-        YArray[i] = '<div class="row">' + XArray.join("\r\n") + '</div>';
+const optionContainer = document.querySelecto('.option-container')
+const flipbutton = document.querySelector('#flipbutton')
+
+let angle = 0
+function flip () {
+const optionShips = console.log(Array.from(optionContainer.children))
+    if(angle === 0){
+        angle = 90
+    } else {
+        angle = 0
     }
-    container.append(YArray.join("\r\n"));
-};
+    optionShips.forEach(optionShip => optionShip.style.transform = 'rotate(${angle}deg)')
+} 
 
-createTable(10, 10);
+flipbutton.addEventListener('click', flip)
+
+
+
+
+
+
+// // https://stackoverflow.com/questions/32772708/how-to-create-a-specific-xx-size-table-battleship-game-html-css-js
+
+// var createTable = function(rows, cols) {
+//     var YArray = new Array(), 
+//         XArray, 
+//             container = $(".table");
+//     for (var i = 0; i < cols; i++) {
+//         XArray = new Array();
+//         for (var j = 0; j < rows; j++) {
+//             XArray[j] = '<div class="cell">&nbsp;</div>';
+//         }
+//         YArray[i] = '<div class="row">' + XArray.join("\r\n") + '</div>';
+//     }
+//     container.append(YArray.join("\r\n"));
+// };
+
+// createTable(10, 10);
